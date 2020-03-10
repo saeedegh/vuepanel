81a057292cab5b10ea6cb52430dbf5d88eceba3d
saeedegh <saeedegholizade1@gmail.com>
2020-03-10 19:20:26 +0330

    installed vue-resource

--- src/main.js
+++ src/main.js
@@ -28,13 +28,14 @@ import Notifications from "./components/NotificationPlugin";
 
 // MaterialDashboard plugin
 import MaterialDashboard from "./material-dashboard";
+import vueResource from 'vue-resource';
 
 import Chartist from "chartist";
 
 // configure router
 const router = new VueRouter({
-  routes, // short for routes: routes
-  linkExactActiveClass: "nav-item active"
+    routes, // short for routes: routes
+    linkExactActiveClass: "nav-item active"
 });
 
 Vue.prototype.$Chartist = Chartist;
@@ -44,13 +45,14 @@ Vue.use(MaterialDashboard);
 Vue.use(GlobalComponents);
 Vue.use(GlobalDirectives);
 Vue.use(Notifications);
+Vue.use(vueResource);
 
 /* eslint-disable no-new */
 new Vue({
-  el: "#app",
-  render: h => h(App),
-  router,
-  data: {
-    Chartist: Chartist
-  }
+    el: "#app",
+    render: h => h(App),
+    router,
+    data: {
+        Chartist: Chartist
+    }
 });
