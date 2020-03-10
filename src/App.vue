fe496616697e43bc585fab496d17a893fd5a250c
saeedegh <saeedegholizade1@gmail.com>
2020-03-11 01:34:19 +0330

    added Cario font

--- src/App.vue
+++ src/App.vue
@@ -5,3 +5,18 @@
 <script>
 export default {};
 </script>
+<style >
+   body,
+        h1,
+        h2,
+        h3,
+        h4,
+        h5,
+        h6,
+        .h1,
+        .h2,
+        .h3,
+        .h4 {
+            font-family: "Cairo" !important;
+        }
+</style>
